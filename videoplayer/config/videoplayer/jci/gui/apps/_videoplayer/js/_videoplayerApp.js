4d01e6ec2c8a30c18523d7e303d4f2ec51a6f06e
Trevor Martin <Trez@mazdatweaks.com>
2018-04-25 16:29:03 -0700

    v3.5
      *   Created starting method StartVideoPlayerApp to start Player instead of using the document 'ready' event
      *   Repeat now toggles none - 1 - all in one button
      *   Shows gplay error in error message
      *   After hitting an error will make up to 3 attempts to re-start the video every 10 seconds
      *   Added Multicontroller 'hold' actions
      *   Video Resumes where it left off when shifting out of reverse
      *   Added to options panel "Resume Play" when checked:
      *   - Reloads saved video list on app reopen and resume (does not scan USB again)
      *   - Resumes the video where you left off if interrupted or app is exited.
      *   - If the video is stopped and you are in the list on exit you will return to the list when reopened.
      *   Multicontroller Hold events
      *   SBNs for toggles

--- videoplayer/config/videoplayer/jci/gui/apps/_videoplayer/js/_videoplayerApp.js
+++ videoplayer/config/videoplayer/jci/gui/apps/_videoplayer/js/_videoplayerApp.js
@@ -8,13 +8,12 @@
 
 log.addSrcFile("_videoplayerApp.js", "_videoplayer");
 
-function _videoplayerApp(uiaId)
-{
-    log.debug("Constructor called.");
+function _videoplayerApp(uiaId) {
+  log.debug("Constructor called.");
 
-    // Base application functionality is provided in a common location via this call to baseApp.init().
-    // See framework/js/BaseApp.js for details.
-    baseApp.init(this, uiaId);
+  // Base application functionality is provided in a common location via this call to baseApp.init().
+  // See framework/js/BaseApp.js for details.
+  baseApp.init(this, uiaId);
 }
 
 
@@ -27,29 +26,39 @@ function _videoplayerApp(uiaId)
  * Called just after the app is instantiated by framework.
  * All variables local to this app should be declared in this function
  */
-_videoplayerApp.prototype.appInit = function()
-{
-    log.debug("_videoplayerApp appInit  called...");
 
+_videoplayerApp.prototype.appInit = function() {
+  log.debug("_videoplayerApp appInit  called...");
+  // These values need to persist through videoplayer instances
+  this.hold = false;
+  this.resumePlay = 0;
+  this.musicIsPaused = false;
+  this.savedVideoList = null;
+  this.resumeVideo = JSON.parse(localStorage.getItem('videoplayer.resumevideo')) || false;
+  this.currentVideoTrack = JSON.parse(localStorage.getItem('videoplayer.currentvideo')) || null;
+  //this.resumePlay = JSON.parse(localStorage.getItem('videoplayer.resume')) || 0;
 
-    //Context table
-    //@formatter:off
+  //Context table
+  //@formatter:off
 
-    this._contextTable = {
-        "Start": { // initial context must be called "Start"
-            "sbName": "Video Player",
-            "template": "VideoPlayerTmplt",
-            "templatePath": "apps/_videoplayer/templates/VideoPlayer", //only needed for app-specific templates
-            "readyFunction": this._StartContextReady.bind(this)
-        } // end of "VideoPlayer"
-    }; // end of this.contextTable object
-    //@formatter:on
+  this._contextTable = {
+    "Start": { // initial context must be called "Start"
+      "sbName": "Video Player",
+      "template": "VideoPlayerTmplt",
+      "templatePath": "apps/_videoplayer/templates/VideoPlayer", //only needed for app-specific templates
+      "readyFunction": this._StartContextReady.bind(this),
+      "contextOutFunction": this._StartContextOut.bind(this),
+      "noLongerDisplayedFunction": this._noLongerDisplayed.bind(this)
+    } // end of "VideoPlayer"
+  }; // end of this.contextTable object
+  //@formatter:on
 
-    //@formatter:off
-    this._messageTable = {
-        // haven't yet been able to receive messages from MMUI
-    };
-    //@formatter:on
+  //@formatter:off
+  this._messageTable = {
+    // haven't yet been able to receive messages from MMUI
+  };
+  //@formatter:on
+  framework.transitionsObj._genObj._TEMPLATE_CATEGORIES_TABLE.VideoPlayerTmplt = "Detail with UMP";
 };
 
 /**
@@ -57,10 +66,34 @@ _videoplayerApp.prototype.appInit = function()
  * CONTEXT CALLBACKS
  * =========================
  */
-_videoplayerApp.prototype._StartContextReady = function ()
-{
+_videoplayerApp.prototype._StartContextReady = function() {
   framework.common.setSbDomainIcon("apps/_videoplayer/templates/VideoPlayer/images/icon.png");
-  framework.transitionsObj._genObj._TEMPLATE_CATEGORIES_TABLE.VideoPlayerTmplt = "Detail with UMP";
+};
+_videoplayerApp.prototype._StartContextOut = function() {
+  framework.common.setSbName('');
+};
+
+_videoplayerApp.prototype._noLongerDisplayed = function() {
+
+  // Stop and close video frame
+  CloseVideoFrame();
+
+  // If we are in reverse then save CurrentVideoPlayTime to resume the video where we left of
+  if ((framework.getCurrentApp() === 'backupparking' || (ResumePlay && CurrentVideoPlayTime > 0)) && this.currentVideoTrack !== null) {
+    this.resumePlay = CurrentVideoPlayTime;
+    localStorage.setItem('videoplayer.resume', JSON.stringify(CurrentVideoPlayTime));
+    CurrentVideoPlayTime = null;
+  }
+  // If we press the 'Entertainment' button we will be running this in the 'usbaudio' context
+  if (!this.musicIsPaused) {
+    setTimeout(function() {
+      if (framework.getCurrentApp() === 'usbaudio') {
+        framework.sendEventToMmui('Common', 'Global.Pause');
+        framework.sendEventToMmui('Common', 'Global.GoBack');
+        this.musicIsPaused = true; // Only run this once
+      }
+    }, 100);
+  }
 };
 
 /**
